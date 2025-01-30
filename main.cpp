c9907f2da5d460ac9cbbc858ce2dee57f5a5eb50
Christian Jair <christian.gomez0831@alumnos.udg.mx>
2025-01-29 18:12:30 -0600

    Mi primer commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**PRESENTACION***
+NOMBRE: Juan Manuel Valdez Lopez
+FECHA: 4 de Febrero de 2025
+PROGRAMA: HolaMundo.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+PROFESOR: Sergio Franco Casillas
+DESCRIPCION: Programa para imprimir un 'Hola, mundo!'
+************************/
+#include <iostream>
+
+int main() {
+    std::cout << "Hola, mundo!" << std::endl;
+    return 0;
+}
