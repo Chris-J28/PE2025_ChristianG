b8d05672dd9e9aa517655638043af1a0061bc8a4
Christian Jair <christian.gomez0831@alumnos.udg.mx>
2025-01-30 16:17:39 -0600

    main 2.c

--- main 2.c
+++ main 2.c
@@ -7,14 +7,14 @@ PROFESOR: Sergio Franco Casillas
 DESCRIPCION: Programa para imprimir datos personales
 ************************/
 #include <stdio.h>
-
+// Inicion del main 
 int main(void)
-{
-    printf("Nombre: Christian Jair Gomez Jimenez\n");
+{ // Comenzamos a pedir que se impriman tods datos necesarios 
+    printf("Nombre: Christian Jair Gomez Jimenez\n"); 
     printf("Edad: 18 años\n");
     printf("Lugar de residencia: Yahualica de Gonzales Gallo\n");
     printf("Fecha: 30 de enero del 2025\n");
     printf("Carrera: Ingenieria en computacion\n");
     printf("Semestre: Segundo semestre\n");
     return 0;
-}
+} // Termino del main
