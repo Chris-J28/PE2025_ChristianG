7ada4fed7b72fb34f0134e34a17d83997e0ba767
Christian Jair <christian.gomez0831@alumnos.udg.mx>
2025-02-06 10:18:08 -0600

    main 4 (Sumar 5 o 10).cpp

--- main 4 (Sumar 5 o 10).cpp
+++ main 4 (Sumar 5 o 10).cpp
@@ -1,17 +1,25 @@
+/**PRESENTACION***
+NOMBRE: Christian Jair Gomez Jimenez
+FECHA: 6 de Febrero de 2025
+PROGRAMA: Sumar 5 0 10.cpp
+CENTRO UNIVERSITARIO DE LOS ALTOS / UNIVERSIDAD DE GUADALAJARA
+PROFESOR: Sergio Franco Casillas
+DESCRIPCION: Programa con el fin de sumar 5 0 10, deoenduendo de la cantidad
+************************/
 #include <iostream>
 #include <cstdio>
 
 using namespace std;
 int numero, i, operaciones=0;
 char respuesta;
-
+//inicio main
 int main() {
-  do {
+  do { //ciclo do-while
     printf("Hola, podrias darme algun numero natural?" );
     scanf("%d", &numero);
-    if (0<numero && numero<500)
+    if (0<numero && numero<500)// ciclo if
     {
-      for (i=1; i<=100; i++) {
+      for (i=1; i<=100; i++) { //ciclo for con contador
         numero +=5;
         printf("Valor actual: %d\n", numero);
         operaciones++;
@@ -19,19 +27,19 @@ int main() {
     }
     else if (500<numero && numero<1000)
     {
-      for (i=1; i<=50; i++) {
+      for (i=1; i<=50; i++) { //ciclo for con contador
         numero +=10;
         printf("Valor actual: %d\n", numero);
         operaciones++;
       }
     }
-    else if (1000<numero) {
+    else if (1000<numero) { 
       printf("No se puede calcular el valor");
 
-  } else {printf("Invalido");}
+  } else {printf("Invalido");} //fin ciclo if
     printf("Numero total de operaciones realizadas: %d\n", operaciones);
     printf("¿Quieres continuar? (S o s para si)");
     scanf(" %c", &respuesta);
-  } while(respuesta == 'S' || respuesta == 's');
-  return 0;
+  } while(respuesta == 'S' || respuesta == 's'); //fin ciclo do-while
+  return 0; //Fin del main
 }
