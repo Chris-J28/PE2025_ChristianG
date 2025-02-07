597c3a36d16bd817cdcb499161ecda27320c921f
Christian Jair <christian.gomez0831@alumnos.udg.mx>
2025-02-06 23:22:32 -0600

    Programacion Estructurada

---
+++ main 5 (cadena de fibonacci).cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int opcion, limite,a = 0, b = 1, temp;
+    char continuar;
+    do {
+        cout << "Ingrese un valor límite para la serie Fibonacci: ";
+        cin >> limite;
+        cout << "Elija el ciclo a usar: 1. for, 2. while, 3. do-while: ";
+        cin >> opcion;
+        cout << "Serie Fibonacci: " << a << " " << b << " ";
+
+        if (opcion == 1) {
+            for (int i = 2; a + b <= limite; i++) {
+                temp = a + b;
+                cout << temp << " ";
+                a = b;
+                b = temp;
+            }
+        } else if (opcion == 2) {
+            while (a + b <= limite) {
+                temp = a + b;
+                cout << temp << " ";
+                a = b;
+                b = temp;
+            }
+        } else if (opcion == 3) {
+            do {
+                temp = a + b;
+                if (temp > limite) break;
+                cout << temp << " ";
+                a = b;
+                b = temp;
+            } while (true);
+        } else {
+            cout << "Opción no válida.\n";
+        }
+
+        cout << endl;
+        cout << "¿Desea continuar? (s/n): ";
+        cin >> continuar;
+    } while (continuar == 's' || continuar == 'S');
+
+    return 0;
+}
